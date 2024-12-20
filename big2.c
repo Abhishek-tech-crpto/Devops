cd1d02827f1b99601873e8245861e8a5d4d2104e
abc <xyz@gmail.com>
2024-12-20 17:42:33 +0000

    c project

---
+++ big2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+
+biggest2() {
+
+    int num1, num2;
+
+
+
+    printf("Enter first number: ");
+
+    scanf("%d", &num1);
+
+
+
+    printf("Enter second number: ");
+
+    scanf("%d", &num2);
+
+
+
+    if (num1 > num2) {
+
+        printf("%d is the bigger number", num1);
+
+    } else {
+
+        printf("%d is the bigger number", num2);
+
+    }
+
+
+
+   // return 0;
+
+} 
+
